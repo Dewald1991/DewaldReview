a82cfecd8f30219fbab94245c90b4a1c8ff91aaa
Dewald Kruger <dewaldk@codeplex.co.za>
2021-05-13 20:07:57 +0200

    Add project files.

---
+++ DewaldReview/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DewaldReview.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DewaldReview/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DewaldReview.WebApiApplication" Language="C#" %>
